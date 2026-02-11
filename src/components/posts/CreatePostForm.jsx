8814b83dcb1596666b96fa680e5301a670b6d519
Caleb Pittman <ambassadoor.dev@gmail.com>
2026-02-11 12:06:08 -0600

    Added new post form

--- src/components/posts/CreatePostForm.jsx
+++ src/components/posts/CreatePostForm.jsx
@@ -6,12 +6,22 @@ import { createPost } from "../../managers/PostManager.js"
 export const CreatePostForm = () => {
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState({error: false, message:""});
-    const userId = useRef()
     const categoryId = useRef()
     const title = useRef()
     const imageUrl = useRef()
     const content = useRef()
     const navigate = useNavigate()
+
+    //TODO: Create a CategoryManager and getCategories function
+    const categories = [{id: 1, label: "Life"}, {id: 2, label: "Work"}, {id: 3, label: "Hobby"}, {id: 4, label: "Fluff"}]
+
+    const handleCancel = () => {
+        if (window.history.state && window.history.state.idx > 0) {
+            navigate(-1)
+        } else {
+            navigate("/")
+        }
+    }
 
     const handleCreateNewPost = (e) => {
         setError({error: false, message: ""})
@@ -19,11 +29,11 @@ export const CreatePostForm = () => {
         setLoading(true)
 
         const newPost = {
-            user_id: userId.current.value(),
-            category_id: categoryId.current.value(),
-            title: title.current.value(),
-            imageUrl: imageUrl.current.value(),
-            content: content.current.value(),
+            user_id: Number(localStorage.getItem("auth_token")),
+            category_id: Number(categoryId.current.value),
+            title: title.current.value,
+            imageUrl: imageUrl.current.value,
+            content: content.current.value,
             approved: true
         }
         createPost(newPost).then(res => {
@@ -36,13 +46,79 @@ export const CreatePostForm = () => {
             } else if (res.status >=500) {
                 setError({error: true, message: "Server error - please try again"})
             } else {
-                setError({error: true, message: "An unexpected error has occured, please try again"})
+                setError({error: true, message: "An unexpected error has occurred, please try again"})
             }
         })
         }
 
+    const errorMessage = (
+            <div className={`modal ${error.error ? "is-active" : ""}`}>
+                <div className="modal-background" onClick={() => setError({error: false, message: ""})}></div>
+                <div className="modal-content">
+                    <div className="box">
+                        <p>{error.message}</p>
+                    </div>
+                </div>
+            </div>
+    )
+
 
     return (
-        <>New Post Form: Coming Soon</>
+        <section className="columns is-centered">
+            <form className="column is-two-thirds" onSubmit={handleCreateNewPost}>
+            <h1 className="title">Rare Publishing</h1>
+                <p className="subtitle">Create New Post</p>
+
+                {error.error && errorMessage}
+
+                <div className="field">
+                    <label className="label">Title*</label>
+                    <div className="control">
+                        <input className="input" type="text" ref={title} required/>
+                    </div>
+                </div>
+
+                <div className="field">
+                    <label className="label">Content*</label>
+                    <div className="control">
+                        <textarea className="textarea" placeholder="Add content here..." ref={content} required></textarea>
+                    </div>
+                </div>
+
+                <div className="field">
+                    <label className="label">Category*</label>
+                    <div className="control">
+                        <select className="select" ref={categoryId} required>
+                            <option value="">Select a Category</option>
+                            {categories.map(cat => (
+                                <option key={cat.id} value={cat.id}>{cat.label}</option>
+                            ))}
+                        </select>
+                    </div>
+                </div>
+
+                <div className="field">
+                    <label className="label">Post Header Image</label>
+                    <div className="control">
+                        <input className="input" type="url" ref={imageUrl}></input>
+                    </div>
+                </div>
+
+                <fieldset disabled={loading}>
+                <div className="field is-grouped">
+                    <div className="control">
+                        <button className="button is-link" type="submit">Submit</button>
+                    </div>
+                    <div className="control">
+                        <button type="button" className="button is-link is-light" onClick={() => navigate("/")}>Cancel</button>
+                    </div>
+                </div>
+                </fieldset>
+                <div className="help">
+                    <p>* Indicates a required field</p>
+                </div>
+
+            </form>
+        </section>
     )
 }
