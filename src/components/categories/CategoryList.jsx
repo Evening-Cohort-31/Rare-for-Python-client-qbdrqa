eae239c41e15e909981d6659ac87cc54fa7715a6
Caleb Pittman <ambassadoor.dev@gmail.com>
2026-02-14 10:48:34 -0600

    add CategoryList component and integrate with routing and navigation

--- src/components/categories/CategoryList.jsx
+++ src/components/categories/CategoryList.jsx
@@ -1,7 +1,47 @@
-export const CategoryList = () => {
+import { useEffect, useState } from "react"
+import { getCategories, createCategory } from "../../managers/CategoryManager.js"
 
+export const CategoryList = () => {
+    const [categories, setCategories] = useState([])
+    const [error, setError] = useState({error: false, message: ""})
+    const [loading, setLoading] = useState(false)
+
+    useEffect(() => {
+        setLoading(true)
+        getCategories().then(res => res.json().then(async res => {
+            setLoading(false)
+            if (res.status === 200) {
+                const response = await res.response
+                setCategories(response)
+            } else if (res.status >=400 && res.status <500) {
+                setError({error: true, message: "Action not supported"})
+            } else if (res.status >=500) {
+                setError({error: true, message: "Server error"})
+            } else {
+                setError({error: true, message: "An unexpected error has occurred"})
+            }
+        })).catch(err => {
+            setLoading(false)
+            setError({error: true, message: "An unexpected error has occurred."})
+        })
+    }, [])
 
     return (
-        <>Categories coming soon!</>
+        <div className="columns is-centered">
+            <div className="column is-one-third">
+                <h1>Categories</h1>
+                {loading ? (
+                    Array.from({ length: 5}).map((_, i) => (
+                        <div className="card is-skeleton" key={i}/>
+                    ))
+                ) : categories.length > 0 ? categories.map(category => (
+                    <div className="card" key={category.id} style={{ marginTop: "10px", minHeight: 50}}>
+                        <header className="card-header">
+                            <p className="card-header-title">${category.label}</p>
+                        </header>
+                    </div>
+                )) : <>No Categories Found</>}
+            </div>
+        </div>
     )
 }
--- src/views/ApplicationViews.js
+++ src/views/ApplicationViews.js
@@ -6,6 +6,7 @@ import { PostsList } from "./PostsList"
 import { MyPosts } from "../components/posts/MyPosts.jsx"
 import { PostForm } from "../components/posts/PostForm.jsx"
 import { ConditionalPostView } from "../components/posts/ConditionalPostView.jsx"
+import { CategoryList } from "../components/categories/CategoryList.jsx"
 
 export const ApplicationViews = ({ token, setToken }) => {
   return (
@@ -20,6 +21,7 @@ export const ApplicationViews = ({ token, setToken }) => {
         <Route path="/new_post" element={<PostForm/>}/>
         <Route path="/post/:id" element={<ConditionalPostView/>}/>
         <Route path="/posts/:userId" element={<MyPosts/>}/>
+        <Route path="/categories" element={<CategoryList/>}/>
 
       </Route>
 
