bbea1dd69693ba35ae7fb98bccabba70a39a528a
Caleb Pittman <ambassadoor.dev@gmail.com>
2026-02-14 11:54:21 -0600

    feat: add category creation functionality to CategoryList component

--- src/components/categories/CategoryList.jsx
+++ src/components/categories/CategoryList.jsx
@@ -3,6 +3,7 @@ import { getCategories, createCategory } from "../../managers/CategoryManager.js
 
 export const CategoryList = () => {
     const [categories, setCategories] = useState([])
+    const [newCategoryLabel, setNewCategoryLabel] = useState("")
     const [error, setError] = useState({error: false, message: ""})
     const [loading, setLoading] = useState(false)
 
@@ -26,21 +27,64 @@ export const CategoryList = () => {
         })
     }, [])
 
+    const handleSubmit = (e) => {
+        setLoading(true)
+        e.preventDefault()
+
+        createCategory({"label": newCategoryLabel}).then(async res => {
+            setLoading(false)
+            setNewCategoryLabel("")
+            const status = res.status
+            if (status === 201) {
+                const response = await res.response
+                setCategories(...categories, response)
+                getCategories().then(async res => setCategories(await res.response))
+            } else if (res.status >=400 && res.status <500) {
+                setError({error: true, message: "Action not supported"})
+            } else if (res.status >=500) {
+                setError({error: true, message: "Server error"})
+            } else {
+                setError({error: true, message: "An unexpected error has occurred."})
+            }
+        })
+    }
+
     return (
-        <div className="columns is-centered">
-            <div className="column is-one-third">
-                <h1>Categories</h1>
-                {loading ? (
-                    Array.from({ length: 5}).map((_, i) => (
-                        <div className="card is-skeleton" key={i}/>
-                    ))
-                ) : categories.length > 0 ? categories.map(category => (
-                    <div className="card" key={category.id} style={{ marginTop: "10px", minHeight: 50}}>
+        <div>
+            <h1 className="title" style={{justifySelf: "center"}}>Categories</h1>
+            <div className="columns" style={{marginInline: 100}}>
+                <div className="column is-half is-offset-one-quarter">
+
+                    {loading ? (
+                        Array.from({ length: 5}).map((_, i) => (
+                            <div className="card is-skeleton" key={i}/>
+                        ))
+                    ) : categories.length > 0 ? categories.map(category => (
+                        <div className="card" key={category.id} style={{ marginTop: "10px", minHeight: 50}}>
+                            <header className="card-header">
+                                <p className="card-header-title">{category.label}</p>
+                            </header>
+                        </div>
+                    )) : <>No Categories Found</>}
+                </div>
+                <div className="column is-one-fourth" style={{marginLeft: 50}}>
+                    <div className="card" style={{marginTop: "10px"}}>
                         <header className="card-header">
-                            <p className="card-header-title">{category.label}</p>
+                            <p className="card-header-title">Create New Category</p>
                         </header>
+                        <div className="card-content">
+                            <div className="field">
+                                <label className="label">Label</label>
+                                <div className="control">
+                                    <input className="input" type="text" value={newCategoryLabel} onChange={(e) => setNewCategoryLabel(e.target.value)}/>
+                                </div>
+                            </div>
+                        </div>
+                        <footer className="card-footer">
+                            <button className="card-footer-item button is-success" onClick={handleSubmit}>Submit</button>
+                        </footer>
                     </div>
-                )) : <>No Categories Found</>}
+                </div>
             </div>
         </div>
     )
