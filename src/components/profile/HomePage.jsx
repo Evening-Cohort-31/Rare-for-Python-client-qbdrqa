86a4ab271842375e43440555c964101027f172e2
Caleb Pittman <ambassadoor.dev@gmail.com>
2026-02-23 20:44:18 -0600

    feat: Enhance HomePage with tabbed navigation and search functionality

--- src/components/profile/HomePage.jsx
+++ src/components/profile/HomePage.jsx
@@ -2,9 +2,11 @@ import { useEffect, useState } from "react"
 import { getHomePage } from "../../managers/UserManager.js"
 import { MyPosts } from "../posts/MyPosts.jsx"
 import { Post } from "../posts/Post.jsx"
+import { BiSearch, BiSearchAlt2 } from "react-icons/bi"
 
 export const HomePage = ({userId}) => {
     const [user, setUser] = useState()
+    const [currentPanelTab, setCurrentPanelTab] = useState(0)
 
     useEffect(() => {
         getHomePage(userId).then(({status, response}) => {
@@ -14,29 +16,54 @@ export const HomePage = ({userId}) => {
         })
     }, [userId])
 
+    const panelTabs = ["All", "My Posts", "Subscriptions"]
+
     return (
         user && 
-        <div className="columns is-centered">
-            <div className="column is-half">
-                <div className="box">
-                    <h2 className="title">My Subscriptions</h2>
-                    <div  style={{overflowY: "scroll", maxHeight: "90vh"}}>
-                        {user.subscribed_posts.map(post => (
-                            <Post post={post} key={post.id} />
-                        ))}
-                    </div>
-                </div>
+        <article className="panel">
+            <p className="panel-heading">{user.username}</p>
+            <div className="panel-block">
+                <p className="control has-icons-left">
+                    <input className="input" type="text" placeholder="Search" />
+                    <span className="icon is-left">
+                        <BiSearchAlt2 />
+                    </span>
+                </p>
             </div>
-            <div className="column is-half">
-                <div className="box">
-                    <h2 className="title">My Posts</h2>
-                    <div  style={{overflowY: "scroll", maxHeight: "80vh"}}>
-                        {user.posts.map(post => (
-                            <Post post={post} key={post.id}/>
-                    ))}
-                    </div>
-                </div>
+            <p className="panel-tabs">
+                {panelTabs.map((tab, i) => (
+                    <button
+                        key={i} 
+                        className={`${currentPanelTab === i ? "is-active has-text-gray" : "has-text-link"}`}
+                        onClick={() => {setCurrentPanelTab(i)}}
+                    >{tab}</button>
+                ))}
+            </p>
+            <div className="panel-block">
+
             </div>
-        </div>
+        </article>
+        // <div className="columns is-centered">
+        //     <div className="column is-half">
+        //         <div className="box">
+        //             <h2 className="title">My Subscriptions</h2>
+        //             <div  style={{overflowY: "scroll", maxHeight: "90vh"}}>
+        //                 {user.subscribed_posts.map(post => (
+        //                     <Post post={post} key={post.id} />
+        //                 ))}
+        //             </div>
+        //         </div>
+        //     </div>
+        //     <div className="column is-half">
+        //         <div className="box">
+        //             <h2 className="title">My Posts</h2>
+        //             <div  style={{overflowY: "scroll", maxHeight: "80vh"}}>
+        //                 {user.posts.map(post => (
+        //                     <Post post={post} key={post.id}/>
+        //             ))}
+        //             </div>
+        //         </div>
+        //     </div>
+        // </div>
     )
 }
