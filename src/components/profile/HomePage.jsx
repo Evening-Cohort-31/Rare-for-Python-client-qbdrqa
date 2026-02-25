1621548b394c3e775b6c7f5be33084807b19b979
Caleb Pittman <ambassadoor.dev@gmail.com>
2026-02-24 18:35:57 -0600

    feat: Enhance HomePage with search functionality and subscription filtering

--- src/components/profile/HomePage.jsx
+++ src/components/profile/HomePage.jsx
@@ -1,14 +1,19 @@
 import { useEffect, useState } from "react"
-import { getHomePage, getUserById } from "../../managers/UserManager.js"
-import { MyPosts } from "../posts/MyPosts.jsx"
+import { getUserById } from "../../managers/UserManager.js"
 import { Post } from "../posts/Post.jsx"
-import { BiSearch, BiSearchAlt2 } from "react-icons/bi"
+import { BiSearchAlt2 } from "react-icons/bi"
 import { getApprovedPublishedPosts, getPostByUserId, getSubscribedPosts } from "../../managers/PostManager.js"
+
+const panelTabs = [{label: "All", method: getApprovedPublishedPosts}, {label: "My Posts", method: getPostByUserId}, {label: "Subscriptions", method: getSubscribedPosts}]
 
 export const HomePage = ({userId}) => {
     const [user, setUser] = useState()
     const [posts, setPosts] = useState([])
+    const [displayedPosts, setDisplayedPosts] = useState([])
     const [currentPanelTab, setCurrentPanelTab] = useState(0)
+    const [currentMenuTab, setCurrentMenuTab] = useState(0)
+    const [searchTerm, setSearchTerm] = useState("")
+
 
     useEffect(() => {
         getUserById(userId).then(({status, response}) => {
@@ -18,29 +23,30 @@ export const HomePage = ({userId}) => {
         })
         panelTabs[currentPanelTab].method(userId).then(({status, response}) => {
             if (status === 200) {
-                response.then(setPosts)
+                response.then((res) => {
+                    setPosts(res)
+                    setDisplayedPosts(res)
+            })
             }
         })
     }, [userId, currentPanelTab])
 
-    const panelTabs = [{label: "All", method: getApprovedPublishedPosts}, {label: "My Posts", method: getPostByUserId}, {label: "Subscriptions", method: getSubscribedPosts}]
+    const filterPosts = (subscriptionId) => {
+        let filteredPosts = posts
+        filteredPosts = posts.filter(post => 
+            post.user.id === subscriptionId
+        )        
+        return filteredPosts
+    }
 
     //TODO: Add loading and error states/handling, add pagination, implement search functionality
     return (
         user && 
         <div className="columns is-centered">
         <article 
-            className="panel column is-two-thirds mt-5"
+            className="panel column is-two-thirds mt-5 is-primary"
         >
             <p className="panel-heading">{user.username}</p>
-            {/* <div className="panel-block">
-                <p className="control has-icons-left">
-                    <input className="input" type="text" placeholder="Search" />
-                    <span className="icon is-left">
-                        <BiSearchAlt2 />
-                    </span>
-                </p>
-            </div> */}
             <p className="panel-tabs">
                 {panelTabs.map((tab, i) => (
                     <button
@@ -50,36 +56,64 @@ export const HomePage = ({userId}) => {
                     >{tab.label}</button>
                 ))}
             </p>
+            <div className="panel-block">
+                <p className="control has-icons-left">
+                    <input 
+                        className="input" 
+                        type="text" 
+                        placeholder="Search" 
+                        value={searchTerm} 
+                        onChange={(e) => {
+                            setSearchTerm(e.target.value)
+                        }}
+                    />
+                    <span className="icon is-left">
+                        <BiSearchAlt2 />
+                    </span>
+                </p>
+            </div>
             <div className="panel-block columns is-centered">
-                <div className="column is-half">
-                    {posts.map(post => (
+                <div className="column is-narrow" style={{marginBottom: "auto", marginTop: 15}}>
+                    <aside className="menu">
+                        {currentPanelTab === 2 && 
+                        <>
+                            <p className="menu-label">Subscriptions</p>
+                            <ul className="menu-list">
+                                <li>
+                                    <button 
+                                        className={`${currentMenuTab === 0 ? "is-active" : ""}`} 
+                                        onClick={() => {
+                                            setCurrentMenuTab(0)
+                                            setDisplayedPosts(posts)
+                                        }}
+                                        >All
+                                    </button>
+                                </li>
+                                {user.subscriptions.map((sub, i) => (
+                                    <li key={sub.id}>
+                                        <button
+                                            className={`${i + 1 === currentMenuTab ? "is-active" : ""}`} 
+                                            onClick={() => {
+                                                setCurrentMenuTab(i + 1)
+                                                setDisplayedPosts(filterPosts(sub.id))
+                                            }}
+                                        >
+                                            {sub.username}
+                                        </button>
+                                    </li>
+                                ))}
+                            </ul>
+                        </>
+                        }
+                    </aside>
+                </div>
+                <div className="column">
+                    {displayedPosts.filter(p => p.title.toLowerCase().includes(searchTerm.toLowerCase())).map(post => (
                         <Post key={post.id} post={post} />
                     ))}
                 </div>
             </div>
         </article>
         </div>
-        // <div className="columns is-centered">
-        //     <div className="column is-half">
-        //         <div className="box">
-        //             <h2 className="title">My Subscriptions</h2>
-        //             <div  style={{overflowY: "scroll", maxHeight: "90vh"}}>
-        //                 {user.subscribed_posts.map(post => (
-        //                     <Post post={post} key={post.id} />
-        //                 ))}
-        //             </div>
-        //         </div>
-        //     </div>
-        //     <div className="column is-half">
-        //         <div className="box">
-        //             <h2 className="title">My Posts</h2>
-        //             <div  style={{overflowY: "scroll", maxHeight: "80vh"}}>
-        //                 {user.posts.map(post => (
-        //                     <Post post={post} key={post.id}/>
-        //             ))}
-        //             </div>
-        //         </div>
-        //     </div>
-        // </div>
     )
 }
