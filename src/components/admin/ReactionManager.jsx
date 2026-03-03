be0c0ca6753f85f9f22f40c609a11a44019bc014
Caleb Pittman <ambassadoor.dev@gmail.com>
2026-03-03 15:44:29 -0600

    feat: Implement Reaction Management UI and fetch reactions from API

--- src/components/admin/ReactionManager.jsx
+++ src/components/admin/ReactionManager.jsx
@@ -1,8 +1,53 @@
-
+import data from '@emoji-mart/data'
+import Picker from '@emoji-mart/react'
+import { useEffect, useState } from 'react'
+import { getReactionOptions } from '../../managers/PostManager.js'
 
 export const ReactionManger = () => {
+    const [reactions, setReactions] = useState([])
 
+    useEffect(() => {
+        getReactionOptions().then(({status, response}) => {
+            if (status === 200) {
+                response.then(setReactions)
+            }
+        })
+    }, [])
+
+    //TODO: Maybe implement some stats to see the most and least popular
     return (
-        <>Management Coming Soon</>
+        <div className='columns is-centered'>
+            <div className='column is-half'>
+                <div className='is-flex is-align-items-center mb-5'>
+                    <h1 className='title mb-0'>Reactions</h1>
+                    <button className='button is-small ml-3 is-success py-1 px-2'>+</button>
+                </div>
+                <table className='table is-striped is-fullwidth'>
+                    <thead>
+                        <tr>
+                            <th>Name</th>
+                            <th>Reaction</th>
+                            <th>Edit</th>
+                            <th>Delete</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {reactions && reactions.map(r => (
+                            <tr>
+                                <td>{r.label}</td>
+                                <td>{r.emoji}</td>
+                                <td>
+                                    <button className='button is-small is-warning'>Edit</button>
+                                </td>
+                                <td>
+                                    <button className='button is-small is-danger'>Delete</button>
+                                </td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
+        </div>
+        //<Picker data={data} onEmojiSelect={console.log}/>
     )
 }
